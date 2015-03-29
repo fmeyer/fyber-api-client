29dc09974934555772edba158322fde940eceb7b
Fernando Meyer <frnmeyer@gmail.com>
2015-03-29 02:03:52 -0400

    Manualy setting RackENV on spechelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,14 @@ SimpleCov.add_filter '/spec/'
 SimpleCov.add_filter '/config/'
 SimpleCov.start if ENV["COVERAGE"]
 
+ENV['RACK_ENV'] = 'test'
+
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment.rb")
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
 
 Capybara.app = Sinatra::Application
 
